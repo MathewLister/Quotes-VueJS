8ec8337cd45367785cf1f8f77210f20fa71ad12f
MathewLister <listermathew0@gmail.com>
2020-09-10 17:10:06 -0700

    can now add new quotes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
-        
+        data: function() {
+            return {
+                quotes: ['Just a sample'],
+                maxQuotes: 10
+            }
+        },
+        methods : {
+            newQuote(quote) {
+                this.quotes.push(quote);
+            }
+        },
+        components: {
+            'app-quote-grid': QuoteGrid,
+            'app-new-quote': NewQuote
+        }
     }
 </script>
 
